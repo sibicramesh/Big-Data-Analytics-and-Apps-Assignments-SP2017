609c86e84fc7b47bfbb21b97f55a855a0ee7a9d0
sibi <sibiterrors@gmail.com>
2017-03-01 23:55:13 -0600

    added code

--- Assignment_6/Source/SparkProgramming/ImageClassification/src/main/scala/ImageClassification_RF.scala
+++ Assignment_6/Source/SparkProgramming/ImageClassification/src/main/scala/ImageClassification_RF.scala
@@ -81,3 +81,35 @@ object ImageClassification_RF {
     println(metrics.confusionMatrix)
   }
 }
+
+import java.io.{File, ByteArrayInputStream}
+import java.net.InetSocketAddress
+import javax.imageio.ImageIO
+
+import com.sun.net.httpserver.{HttpExchange, HttpHandler, HttpServer}
+import sun.misc.BASE64Decoder
+
+object SimpleHttpServer extends App{
+  val server = HttpServer.create(new InetSocketAddress("192.168.1.218",8080), 0)
+  server.createContext("/get", new RootHandler())
+  server.setExecutor(null)
+  server.start()
+  println("------ waiting for Request ------")
+}
+
+class RootHandler extends HttpHandler {
+  def handle(httpExchange: HttpExchange) {
+
+
+    println("------ Image receiving complete ------")
+
+    val response = "33%";
+
+    httpExchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*")
+    httpExchange.sendResponseHeaders(200, response.length())
+    val outStream = httpExchange.getResponseBody
+    outStream.write(response.getBytes)
+    outStream.close()
+  }
+}
+
